e470f1fa8ac1f8e62ffc7e370f31393628c3b527
Dutra3 <gabrieldutra45@gmail.com>
2023-11-11 02:13:23 -0300

    Create POST method

--- new-server/server.js
+++ new-server/server.js
@@ -1,14 +1,21 @@
 import { fastify } from 'fastify';
 import { DatabaseMemory } from './database.memory.js';
 
-const server = fastify()
+const server = fastify();
+const database = new DatabaseMemory();
 
 server.get('/videos', () => {
     return 'Hello World';
 })
 
-server.post('/videos', () => {
-    return 'Hello World';
+server.post('/videos', (request, reply) => {
+    database.create({
+        title: 'Video 01',
+        description: 'Description Video 01',
+        duration: 180,
+    })
+
+    return reply.status(201).send();
 })
 
 server.put('/videos/:id', () => {
