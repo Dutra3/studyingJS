1b02495364118522f452f1c07892b919564ad79c
Dutra3 <gabrieldutra45@gmail.com>
2023-11-12 03:30:19 -0300

    Fix create table and create method to use postgres as main db

--- new-server/server.js
+++ new-server/server.js
@@ -1,20 +1,22 @@
 import { fastify } from 'fastify';
-import { DatabaseMemory } from './database.memory.js';
+// import { DatabaseMemory } from './database.memory.js';
+import { DatabasePostgres } from './database-postgres.js';
 
 const server = fastify();
-const database = new DatabaseMemory();
+// const database = new DatabaseMemory();
+const database = new DatabasePostgres();
 
-server.get('/videos', () => {
+server.get('/videos', async (request) => {
     const search = request.query.search;
-    const videos = database.list(search);
+    const videos = await database.list(search);
 
     return videos;
 })
 
-server.post('/videos', (request, reply) => {
+server.post('/videos', async (request, reply) => {
     const { title, description, duration } = request.body;
     
-    database.create({
+    await database.create({
         title,
         description,
         duration,
@@ -23,11 +25,11 @@ server.post('/videos', (request, reply) => {
     return reply.status(201).send();
 })
 
-server.put('/videos/:id', (request, reply) => {
+server.put('/videos/:id', async (request, reply) => {
     const videoId = request.params.id;
     const { title, description, duration } = request.body;
 
-    database.update(videoId, {
+    await database.update(videoId, {
         title,
         description,
         duration,
