48c39b7d587e14e6d0d675ef4c8785cd492d0765
Gabriel Dutra <gabrieldutra45@gmail.com>
2025-01-08 01:11:00 -0300

    Create a simple example of jwt authentication with fiber

---
+++ go-middleware/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"time"
+
+	jwtware "github.com/gofiber/contrib/jwt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", public)
+
+	app.Post("/login", login)
+
+	app.Use(jwtware.New(jwtware.Config {
+		SigningKey: jwtware.SigningKey{Key: []byte("super_secret")},
+	}))
+
+	app.Get("/restricted", restricted)
+
+	app.Get("/admin", admin)
+
+	app.Listen(":3000")
+}
+
+func admin(c *fiber.Ctx) error {
+	return c.SendString("Hello admin")
+}
+
+func restricted(c *fiber.Ctx) error {
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	name := claims["name"].(string)
+	
+	return c.SendString("Welcome " + name)
+}
+
+func login(c *fiber.Ctx) error {
+	user := c.FormValue("user")
+	pass := c.FormValue("pass")
+
+	if user != "jhon" || pass != "doe" {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+
+	claims := jwt.MapClaims {
+		"name": "John Doe",
+		"admin": true,
+		"exp": time.Now().Add(time.Hour * 72).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	t, err := token.SignedString([]byte("super_secret"))
+	
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(fiber.Map{"token": t})
+}
+
+func public(c *fiber.Ctx) error {
+	return c.SendString("THIS IS PUBLIC")
+}
